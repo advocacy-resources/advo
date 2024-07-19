c25aef63303467369dccfc3795d36256b86a1782
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-07-19 17:30:35 -0600

    cleaning up unnecessary imports.

--- advo/src/app/page.tsx
+++ advo/src/app/page.tsx
@@ -1,11 +1,9 @@
-import Image from "next/image";
 import Navbar from "../components/Navbar";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
-      <h1>Hello World!</h1>
     </main>
   );
 }
