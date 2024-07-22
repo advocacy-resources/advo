9ed673e6070a2d3652e52f3a6fe6013d2f370d00
kbramlett <75955180+kbramlett@users.noreply.github.com>
2024-07-21 19:42:07 -0700

    feat: :sparkles: add content to footer

--- advo/src/app/page.tsx
+++ advo/src/app/page.tsx
@@ -1,9 +1,12 @@
 import Navbar from "../components/Navbar";
+import Footbar from "@/components/Footbar";
 
 export default function Home() {
   return (
     <main className="">
       <Navbar />
+      <Footbar />
     </main>
+
   );
 }
