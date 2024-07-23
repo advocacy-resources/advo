4f81cab95a99527a9898a2d8afc42d0c3a17a1ba
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-07-23 15:08:28 -0600

    add auth config

--- advo/src/app/api/auth/[...nextauth]/route.ts
+++ advo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,15 +1,48 @@
-import NextAuth from "next-auth/next";
-import GoogleProvider from "next-auth/providers/google";
+// app/api/auth/[...nextauth]/route.ts
+import NextAuth, { SessionStrategy, User } from 'next-auth';
+import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../../lib/db';
+import { compare } from 'bcryptjs';
 
-export const authOptions = {
-    providers: [
-        GoogleProvider({
-          clientId: process.env.GOOGLE_CLIENT_ID,
-          clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ]
-}
+const authOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        username: { label: 'Username', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      authorize: async (credentials): Promise<User | null> => {
+        const client = await clientPromise;
+        const db = client.db();
 
-export const handler = NextAuth(authOptions)
+        const user = await db.collection('users').findOne({ username: credentials?.username });
+        if (user && credentials?.password) {
+          const isValid = await compare(credentials.password, user.password);
+          if (isValid) {
+            return { id: user._id.toString(), name: user.name, email: user.email };
+          }
+        }
+        return null;
+      },
+    }),
+  ],
+  adapter: MongoDBAdapter(Promise.resolve(clientPromise)),
+  secret: process.env.SECRET,
+  session: {
+    strategy: 'jwt' as SessionStrategy,
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
+};
+
+const handler = NextAuth(authOptions);
 
 export { handler as GET, handler as POST };
