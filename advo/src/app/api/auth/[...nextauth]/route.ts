b24b70b47937fc4eb1e44c3deae738288f191232
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-07-24 08:02:38 -0600

    feat: create login auth api route

--- advo/src/app/api/auth/[...nextauth]/route.ts
+++ advo/src/app/api/auth/[...nextauth]/route.ts
@@ -1,10 +1,12 @@
 // app/api/auth/[...nextauth]/route.ts
-import NextAuth, { SessionStrategy, User } from 'next-auth';
+import NextAuth, { SessionStrategy } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
-import clientPromise from '../../../../lib/db';
 import { compare } from 'bcryptjs';
+
+import clientPromise from '@/lib/db';
+import { IUser, IUserLogin } from "&/user";
 
 const authOptions = {
   providers: [
@@ -18,15 +20,21 @@ const authOptions = {
         username: { label: 'Username', type: 'text' },
         password: { label: 'Password', type: 'password' },
       },
-      authorize: async (credentials): Promise<User | null> => {
+      authorize: async (credentials): Promise<IUser | null> => {
         const client = await clientPromise;
         const db = client.db();
 
-        const user = await db.collection('users').findOne({ username: credentials?.username });
+        const user: IUserLogin | null = await db.collection('users').findOne({ username: credentials?.username }) as unknown as IUserLogin | null;
         if (user && credentials?.password) {
           const isValid = await compare(credentials.password, user.password);
           if (isValid) {
-            return { id: user._id.toString(), name: user.name, email: user.email };
+            return {
+              id: user._id.toString(),
+              name: user.name,
+              email: user.email,
+              image: user.image,
+              emailVerified: user.emailVerified,
+            };
           }
         }
         return null;
@@ -40,6 +48,7 @@ const authOptions = {
   },
   pages: {
     signIn: '/auth/signin',
+    newUser: '/account',
   },
 };
 
