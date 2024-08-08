2a78f7a7c4fc96cdf8abd4dca64879b509f4da9b
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-08-07 18:18:33 -0600

    chore: Add Toaster component for displaying toast notifications

--- advo/src/app/layout.tsx
+++ advo/src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { getServerSession } from "next-auth";
 import SessionProvider from "#/utils/SessionProvider";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -22,6 +23,7 @@ export default async function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <SessionProvider session={session}>{children}</SessionProvider>
+        <Toaster />
       </body>
     </html>
   );
--- advo/src/app/page.tsx
+++ advo/src/app/page.tsx
@@ -1,12 +1,17 @@
+"use client";
+
 import Footbar from "@/components/footbar/Footbar";
 import Navbar from "@/components/navbar/Navbar";
 import Search from "@/components/search/MainSearch";
-import Modal from "@/components/footbar/TermsOfService";
+import { toast } from "@/components/ui/use-toast";
+import { Button } from "@/components/ui/button";
+import CookieNotice from "@/components/utils/CookieNotice";
 
 export default function Home() {
   return (
     <main className="flex flex-col justify-between h-screen">
       <Navbar />
+      <CookieNotice />
       <Search />
       <Footbar />
     </main>
