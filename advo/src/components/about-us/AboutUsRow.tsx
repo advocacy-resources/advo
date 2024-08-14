e4f44799bf013b7f2c98d0768f22b8200bb3b934
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-08-14 14:21:28 -0600

    remove blank line.

--- advo/src/components/about-us/AboutUsRow.tsx
+++ advo/src/components/about-us/AboutUsRow.tsx
@@ -27,7 +27,6 @@ const AboutUsRow: React.FC<AboutUsRow> = ({
           height={600} // Adjust based on your actual image height
           className="w-full h-auto rounded-md"
         />
-
       </div>
       <div className="md:w-2/3 w-full p-4">
         <h2 className="text-xl font-bold mb-2">{title}</h2>
