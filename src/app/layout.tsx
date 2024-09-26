1f2a26a0aecd8426147cadda527dbc5d033fbe78
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-09-26 12:59:51 -0500

    Add @vercel/speed-insights package import to layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import "./globals.css";
 import NextAuthProvider from "@/components/utils/SessionProvider";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function RootLayout({
   children,
