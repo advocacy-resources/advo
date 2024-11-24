75997a637d5271d61b550221ec0c6e5df0084662
K. M. Jones-Eastland <117540963+kmje405@users.noreply.github.com>
2024-11-23 20:30:41 -0600

    adding gtfo button to navbar

--- src/components/layout/Navbar.tsx
+++ src/components/layout/Navbar.tsx
@@ -22,17 +22,36 @@ function Navbar() {
 
   return (
     <header>
-      <div className="relative h-16">
-        <Image
-          src={Logo}
-          alt="The myAdvo Logo."
-          height={60}
-          className="absolute flex justify-center h-full w-full py-2 z-1"
-          onClick={() => {
-            router.push("/");
-          }}
-        />
-        <div className="absolute right-0 flex justify-end items-center px-8 h-full -z-1">
+      <div className="relative h-16 flex items-center">
+        {/* Left Icon */}
+        <div className="absolute left-0 flex items-center px-4 h-full rotate-180">
+          <Image
+            src="/images/gtfo.svg"
+            alt="Navigation Icon"
+            height={24}
+            width={24}
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/somepage"); // Replace with your desired route
+            }}
+          />
+        </div>
+
+        {/* Center Logo */}
+        <div className="absolute inset-0 flex justify-center items-center">
+          <Image
+            src={Logo}
+            alt="The myAdvo Logo."
+            height={60}
+            className="cursor-pointer"
+            onClick={() => {
+              router.push("/");
+            }}
+          />
+        </div>
+
+        {/* Right Section */}
+        <div className="absolute right-0 flex justify-end items-center px-8 h-full">
           {session ? (
             <Button onClick={handleSignOut}>Sign Out</Button>
           ) : (
