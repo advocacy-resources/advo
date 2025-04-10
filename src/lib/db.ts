e30d280ec9580af3c3a4608968a1bb18cb5597f9
kmje405 <kmje405@gmail.com>
2025-04-10 15:58:15 -0500

    fix: update database connection to use development environment and modify install command for compatibility

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -36,6 +36,4 @@ if (process.env.NODE_ENV === "development") {
 // separate module, the client can be shared across functions.
 export default client;
 
-export const db = client.db(
-  process.env.NODE_ENV === "production" ? "advo-prod" : "advo-dev",
-);
+export const db = client.db("advo-dev");
